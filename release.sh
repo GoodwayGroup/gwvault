7ea760cc450726734ae29bcf2d045dbe7ee476d6
Derek Smith <drsmith.phys@gmail.com>
2020-07-19 15:02:32 -0500

    devops: updated release process and changelog to git-chglog

--- release.sh
+++ release.sh
@@ -1,26 +1,85 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set +e
+set -o noglob
+
+#
+# Set Colors
+#
+
+bold="\e[1m"
+dim="\e[2m"
+underline="\e[4m"
+blink="\e[5m"
+reset="\e[0m"
+red="\e[31m"
+green="\e[32m"
+blue="\e[34m"
+
+#
+# Common Output Styles
+#
+
+h1() {
+  printf "\n${bold}${underline}%s${reset}\n" "$(echo "$@" | sed '/./,$!d')"
+}
+h2() {
+  printf "\n${bold}%s${reset}\n" "$(echo "$@" | sed '/./,$!d')"
+}
+info() {
+  printf "${dim}➜ %s${reset}\n" "$(echo "$@" | sed '/./,$!d')"
+}
+success() {
+  printf "${green}✔ %s${reset}\n" "$(echo "$@" | sed '/./,$!d')"
+}
+error() {
+  printf "${red}${bold}✖ %s${reset}\n" "$(echo "$@" | sed '/./,$!d')"
+}
+warnError() {
+  printf "${red}✖ %s${reset}\n" "$(echo "$@" | sed '/./,$!d')"
+}
+warnNotice() {
+  printf "${blue}✖ %s${reset}\n" "$(echo "$@" | sed '/./,$!d')"
+}
+note() {
+  printf "\n${bold}${blue}Note:${reset} ${blue}%s${reset}\n" "$(echo "$@" | sed '/./,$!d')"
+}
+
+typeExists() {
+  if [ $(type -P $1) ]; then
+    return 0
+  fi
+  return 1
+}
+
+if ! typeExists "git-chglog"; then
+  error "git-chglog is not installed"
+  note "To install run: go get -u github.com/git-chglog/git-chglog/cmd/git-chglog"
+  exit 1
+fi
 
 VERSION=${1}
 
 if [ "x${VERSION}x" = "xx" ]; then
-    echo "Must supply version number as first argument"
-    exit 1
+  error "Must supply version number as first argument"
+  exit 1
 fi
 
-echo "Updating CHANGELOG.md"
-auto-changelog -v $VERSION -l false --template keepachangelog && git add CHANGELOG.md
-git commit -m "$VERSION"
+h1 "Preparing release of $VERSION"
 
-echo "Tagging version: $VERSION"
+h2 "Tagging version: $VERSION"
 git tag $VERSION
 
-echo "Building assets to be uploaded (currently manual process)"
+h2 "Updating CHANGELOG.md"
+git-chglog -o CHANGELOG.md && git add CHANGELOG.md
+git commit -m "feat(release): $VERSION"
+
+note "Building assets to be uploaded"
 make ci
 
 echo ""
-echo "What you still need to do:"
-echo "1. Push the tag: git push origin $VERSION"
-echo "2. Update the release in github"
-echo "3. Add the assets to the release."
+note "What you still need to do:"
+info "1. Push the tag: git push origin $VERSION"
+info "2. Update the release in github with compiled assets."
 echo ""
-echo "Done!"
+success "Done!"
